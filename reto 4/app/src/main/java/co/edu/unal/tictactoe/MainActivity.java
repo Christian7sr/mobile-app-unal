51e89b4c90940e86a1901e198b2d43392eed935e
Christian <cdsanabriar@unal.edu.co>
2019-09-24 22:38:06 -0500

    upload the 4 challenge

---
+++ reto 4/app/src/main/java/co/edu/unal/tictactoe/MainActivity.java
@@ -0,0 +1,125 @@
+package co.edu.unal.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add("New Game");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        startNewGame();
+        mGameOver = false;
+        return true;
+    }
+
+    boolean mGameOver;
+
+    // Represents the internal state of the game
+    private TicTacToeGame mGame;
+
+    // Buttons making up the board
+    private Button mBoardButtons[];
+
+    // Various text displayed
+    private TextView mInfoTextView;
+
+    // Set up the game board.
+    private void startNewGame() {
+
+        mGame.clearBoard();
+
+        // Reset all buttons
+        for (int i = 0; i < mBoardButtons.length; i++) {
+            mBoardButtons[i].setText("");
+            mBoardButtons[i].setEnabled(true);
+            mBoardButtons[i].setOnClickListener(new ButtonClickListener(i));
+        }
+
+        // Human goes first
+        mInfoTextView.setText(R.string.first_human);
+
+    }    // End of startNewGame
+
+    public void setMove(char player, int location){
+
+        mGame.setMove(player, location);
+        mBoardButtons[location].setEnabled(false);
+        mBoardButtons[location].setText(String.valueOf(player));
+        if (player == TicTacToeGame.HUMAN_PLAYER)
+            mBoardButtons[location].setTextColor(Color.rgb(0, 200, 0));
+        else
+            mBoardButtons[location].setTextColor(Color.rgb(200, 0, 0));
+    }
+
+    // Handles clicks on the game board buttons
+    class ButtonClickListener implements View.OnClickListener {
+        int location;
+
+        public ButtonClickListener(int location) {
+            this.location = location;
+        }
+
+        public void onClick(View view) {
+            if (mBoardButtons[location].isEnabled()) {
+                setMove(TicTacToeGame.HUMAN_PLAYER, location);
+
+                // If no winner yet, let the computer make a move
+                int winner = mGame.checkForWinner();
+                if (winner == 0) {
+                    mInfoTextView.setText(R.string.turn_computer);
+                    int move = mGame.getComputerMove();
+                    setMove(TicTacToeGame.COMPUTER_PLAYER, move);
+                    winner = mGame.checkForWinner();
+                }
+
+                if (winner == 0)
+                    mInfoTextView.setText(R.string.turn_human);
+                else if (winner == 1)
+                    mInfoTextView.setText(R.string.result_tie);
+                else if (winner == 2)
+                    mInfoTextView.setText(R.string.result_human_wins);
+                else
+                    mInfoTextView.setText(R.string.result_computer_wins);
+            }
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBoardButtons = new Button[TicTacToeGame.BOARD_SIZE];
+        mBoardButtons[0] = (Button) findViewById(R.id.one);
+        mBoardButtons[1] = (Button) findViewById(R.id.two);
+        mBoardButtons[2] = (Button) findViewById(R.id.three);
+        mBoardButtons[3] = (Button) findViewById(R.id.four);
+        mBoardButtons[4] = (Button) findViewById(R.id.five);
+        mBoardButtons[5] = (Button) findViewById(R.id.six);
+        mBoardButtons[6] = (Button) findViewById(R.id.seven);
+        mBoardButtons[7] = (Button) findViewById(R.id.eight);
+        mBoardButtons[8] = (Button) findViewById(R.id.nine);
+
+        mInfoTextView = (TextView) findViewById(R.id.information);
+
+        mGame = new TicTacToeGame();
+
+        startNewGame();
+    }
+}
